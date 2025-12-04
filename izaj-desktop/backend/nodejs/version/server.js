aa2f6fc527037658753b5c971fdde0825ce09038
Ruiz Miguel Sapio <161678318+Alamatniige@users.noreply.github.com>
2025-12-04 15:14:21 +0800

    adjust paulit ulit

--- izaj-desktop/backend/nodejs/version/server.js
+++ izaj-desktop/backend/nodejs/version/server.js
@@ -6,7 +6,7 @@ const LATEST_VERSION = '1.0.7';
 const PRODUCT_NAME = 'Izaj Lighting Centre';
 const GITHUB_USERNAME = 'Alamatniige';
 const GITHUB_REPO = 'izaj-desktop-application';
-const DOWNLOAD_BASE_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/releases/download/`;
+const DOWNLOAD_BASE_URL = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO}/tree/main/releases/downloads/`;
 
 const compareVersions = (v1, v2) => {
   const v1Parts = v1.split('.').map(Number);
